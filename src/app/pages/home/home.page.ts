6fa682e2ce3ad0479426cb224edfe9201a65847e
Nhok95 <enrique_1095@hotmail.es>
2021-01-09 20:39:05 +0100

    Fix infinite scroll header name

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -26,7 +26,7 @@ export class HomePage implements OnInit {
     },
     {
       icon: 'infinite-outline',
-      name: 'Infinites-Scroll',
+      name: 'Infinite-Scroll',
       redirectTo: '/infinite'
     }
   ];
