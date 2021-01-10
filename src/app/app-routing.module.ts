3a4adb46d140fe061bb0120465b07609a6e75696
Nhok95 <enrique_1095@hotmail.es>
2021-01-10 12:51:01 +0100

    Added Input

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'input',
+    loadChildren: () => import('./pages/input/input.module').then( m => m.InputPageModule)
   }
+
   
 ];
 
