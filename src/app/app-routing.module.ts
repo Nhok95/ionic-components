7ed7453bed8b188c53f91ed2a493496c18639307
Nhok95 <enrique_1095@hotmail.es>
2021-01-11 15:52:33 +0100

    Added Progress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,11 @@ const routes: Routes = [
   {
     path: 'action-sheet',
     loadChildren: () => import('./pages/action-sheet/action-sheet.module').then( m => m.ActionSheetPageModule)
+  },  {
+    path: 'progress',
+    loadChildren: () => import('./pages/progress/progress.module').then( m => m.ProgressPageModule)
   }
+
   
 ];
 
