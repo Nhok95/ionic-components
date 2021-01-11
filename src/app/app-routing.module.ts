f07f5a36b4db8b67aa2f97ab46fcccaf10ee670d
Nhok95 <enrique_1095@hotmail.es>
2021-01-11 15:38:47 +0100

    Added Popover

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,11 @@ const routes: Routes = [
   {
     path: 'action-sheet',
     loadChildren: () => import('./pages/action-sheet/action-sheet.module').then( m => m.ActionSheetPageModule)
+  },  {
+    path: 'popover',
+    loadChildren: () => import('./pages/popover/popover.module').then( m => m.PopoverPageModule)
   }
+
   
 ];
 
