66d34a4c45628df15590c00f504e99fce018bf96
Nhok95 <enrique_1095@hotmail.es>
2021-01-14 16:30:57 +0100

    Added Slides

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,11 @@ const routes: Routes = [
   {
     path: 'action-sheet',
     loadChildren: () => import('./pages/action-sheet/action-sheet.module').then( m => m.ActionSheetPageModule)
+  },  {
+    path: 'slides',
+    loadChildren: () => import('./pages/slides/slides.module').then( m => m.SlidesPageModule)
   }
+
   
 ];
 
